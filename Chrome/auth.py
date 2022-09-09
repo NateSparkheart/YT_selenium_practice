5ae9ee5944c0e22b21b093e74ae22b669caf9e61
Nate <kappavlad25@gmail.com>
2022-09-09 22:18:43 +0300

    Добавлен код для сохранения cookie

--- Chrome/auth.py
+++ Chrome/auth.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from auth_data import vk_login, vk_password
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+#from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get('https://vk.com/')
@@ -13,9 +13,9 @@ driver.get('https://vk.com/')
 
 login_field = driver.find_element(By.ID, "index_email")
 login_field.send_keys(vk_login)
-login_field.send_keys(Keys.ENTER)
-#driver.find_element(By.CSS_SELECTOR, "button.FlatButton.FlatButton--primary.FlatButton--size-l.FlatButton--wide"
-                                     #".VkIdForm__button.VkIdForm__signInButton > span").click()
+#login_field.send_keys(Keys.ENTER)
+driver.find_element(By.CSS_SELECTOR, "button.FlatButton.FlatButton--primary.FlatButton--size-l.FlatButton--wide"
+                                     ".VkIdForm__button.VkIdForm__signInButton > span").click()
 
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button > span.vkuiButton__in")))
 driver.find_element(By.CSS_SELECTOR, '[name="password"]').send_keys(vk_password)
---
+++ Firefox/cookies_Firefox.py
@@ -0,0 +1,38 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from auth_data import vk_login, vk_password
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pickle
+
+driver = webdriver.Firefox()
+
+driver.get('https://vk.com/')
+
+
+# login_field = driver.find_element(By.ID, "index_email")
+# login_field.send_keys(vk_login)
+#
+# driver.find_element(By.CSS_SELECTOR, "button.FlatButton.FlatButton--primary.FlatButton--size-l.FlatButton--wide"
+#                                      ".VkIdForm__button.VkIdForm__signInButton > span").click()
+#
+# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button > span.vkuiButton__in")))
+# driver.find_element(By.CSS_SELECTOR, '[name="password"]').send_keys(vk_password)
+# driver.find_element(By.CSS_SELECTOR, "button > span.vkuiButton__in").click()
+#
+# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#l_nwsf > a > span.left_label.inl_bl")))
+# driver.find_element(By.CSS_SELECTOR, "#l_nwsf > a > span.left_label.inl_bl").click()
+#
+# time.sleep(3)
+# pickle.dump(driver.get_cookies(), open(f"{vk_login}_cookies_Firefox", "wb"))
+
+for cookie in pickle.load(open(f"{vk_login}_cookies_Firefox", "rb")):
+    driver.add_cookie(cookie)
+
+
+time.sleep(2)
+driver.refresh()
+time.sleep(5)
+
+driver.quit()
